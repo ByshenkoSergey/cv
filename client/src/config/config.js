b8a2d73ed55e992bd44bdda7dd7ef6294c5cbc9e
Serhii Byshenko <byshenkoS1989@gmail.com>
2024-04-22 13:16:19 +0300

    Change url of server

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
 
-    backendUrl: "http://localhost:8080/api/v1"
+    backendUrl: "https://cv-8tlw.onrender.com/api/v1"
     
     // Please replase url if you want deploy into prodaction
-    // prodaction url: https://dalle-network.onrender.com/api/v1
+    // prodaction url: https://cv-8tlw.onrender.com/api/v1
     // local url: http://localhost:8080/api/v1
   };
   
